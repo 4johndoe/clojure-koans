508585436c431f8be0749ff9d245f3820e7ea486
Демченко Богдан <co_dbv@gks.local>
2017-09-14 20:10:37 +0300

    func square

--- src/koans/13_creating_functions.clj
+++ src/koans/13_creating_functions.clj
@@ -18,13 +18,13 @@
           (multiply-by-5 4)))
 
   "Don't forget: first things first"
-  (= [__ __ __ __]
+  (= [:a :b :c :d]
        (let [ab-adder (partial concat [:a :b])]
-         (ab-adder [__ __])))
+         (ab-adder [:c :d])))
 
   "Functions can join forces as one 'composed' function"
   (= 25 (let [inc-and-square (comp square inc)]
-          (inc-and-square __)))
+          (inc-and-square 4)))
 
   "Have a go on a double dec-er"
   (= __ (let [double-dec (comp dec dec)]
